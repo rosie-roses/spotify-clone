a1ce73ec6ca848601cf7da351e17fd20ee4f9dd5
Rosanne Saparamadu <rosanne.thms@gmail.com>
2024-10-12 22:31:35 +1300

    Completed NextAuth configuration and callbacks, refreshing access token.

---
+++ pages/login.jsx
@@ -0,0 +1,12 @@
+import { signIn } from 'next-auth/react';
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex items-center justify-center'>
+        <button onClick={() => signIn('spotify', { callbackUrl: '/' })}>Login with Spotify</button>
+    </div>
+  )
+}
+
+export default Login;
